88b01602146cf6036813a646e9699ee651ca40c3
szcharlesji <39901664+szcharlesji@users.noreply.github.com>
2023-07-21 09:44:18 +0000

    finish vec3.h

--- vec3.h
+++ vec3.h
@@ -74,4 +74,48 @@ inline vec3 operator+(const vec3& u, const vec3& v) // Add
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
+inline vec3 operator-(const vec3& u, const vec3& v) // Subtract
+{
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) // Multiply
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) // Multiply by scalar
+{
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) // Multiply by scalar
+{
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) // Divide by scalar
+{
+    return (1/t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) // Dot product
+{
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) // Cross product
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) // Unit vector
+{
+    return v / v.length();
+}
+
 #endif // !VEC3_H
