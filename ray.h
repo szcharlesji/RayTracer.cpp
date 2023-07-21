f68ae552c1653beecbbbdfc17252e9ada133bec8
szcharlesji <39901664+szcharlesji@users.noreply.github.com>
2023-07-21 10:49:25 +0000

    initialize ray.h

---
+++ ray.h
@@ -0,0 +1,26 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+    public:
+        ray() {} // Default constructor
+        ray(const point3& origin, const vec3& direction)
+            : orig(origin), dir(direction)
+        {} // Overload constructor with origin and direction
+
+        point3 origin() const { return orig; }
+        vec3 direction() const { return dir; }
+
+        point3 at(double t) const
+        {
+            return orig + t * dir;
+        }
+
+    public:
+        point3 orig;
+        vec3 dir;
+};
+
+#endif
